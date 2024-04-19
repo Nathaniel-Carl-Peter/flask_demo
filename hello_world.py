018df83b779abcab749bcd280c40048745a2d2f5
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-04-19 23:54:47 +1000

    add temp function

--- hello_world.py
+++ hello_world.py
@@ -14,5 +14,14 @@ def greet(name=""):
     return f"Hello {name}"
 
 
+@app.route('/greet')
+@app.route('Convert celsius')
+def get_fahrenheit():
+    """Convert to fahrenheit"""
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f"Result: {celsius:.2f} C")
+
+
 if __name__ == '__main__':
     app.run()
